7fe18a471eba6c46ce2c9177a9563d2afb01d003
FineLime <42850488+FineLime@users.noreply.github.com>
2019-11-15 23:01:43 +0000

    Update slots.py

--- commands/slots.py
+++ commands/slots.py
@@ -13,7 +13,7 @@ class Slots(commands.Cog):
     @commands.cooldown(1, 10, BucketType.user) 
     async def slotsleaderboard(self, ctx): 
         leaderboard = await self.client.pg_con.fetch("SELECT * FROM users ORDER BY slotwins DESC")
-        await ctx.send(f"```1) {leaderboard[0]['name']}\n2) {leaderboard[1]['name']}\n3) {leaderboard[2]['name']}```")
+        await ctx.send(f"```1) {leaderboard[0]['name']} - {leaderboard[0]['slotwins']}\n2) {leaderboard[1]['name']} - {leaderboard[1]['slotwins']}\n3) {leaderboard[2]['name']} - {leaderboard[2]['slotwins']}```")
     
     @commands.command()
     @commands.cooldown(1, 10, BucketType.user) 
