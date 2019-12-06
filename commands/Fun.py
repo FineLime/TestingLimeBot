0794c6533277d268cd9186a45723ff29cb008144
FineLime <42850488+FineLime@users.noreply.github.com>
2019-12-06 23:56:52 +0000

    Update Fun.py

--- commands/Fun.py
+++ commands/Fun.py
@@ -15,7 +15,7 @@ class Fun(commands.Cog):
             user = ctx.author
         
         embed = discord.Embed(title=user.mention, color=0x00ff00)
-        embed.set_image(url=user.url)
+        embed.set_image(url=user.avatar_url)
         embed.set_footer(text=f"Requested by {ctx.author.mention}")
         await get(ctx.guild.channels, id=int(server[0]["logschannel"])).send(embed=embed)
 
