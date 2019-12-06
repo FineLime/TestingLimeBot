78a337b27d9d90e3436606deeae7c7c6135d18a4
FineLime <42850488+FineLime@users.noreply.github.com>
2019-12-06 23:53:14 +0000

    Create Fun.py

---
+++ commands/Fun.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+
+class Fun(commands.Cog): 
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.cooldown(1, 10, BucketType.user)
+    async def avatar(self, ctx, user:discord.Member = "None"): 
+        
+        if user == "None": 
+            user = ctx.author
+        
+        embed = discord.Embed(title=user.mention, color=0x00ff00)
+        embed.set_image(url=user.url)
+        embed.set_footer(text=f"Requested by {ctx.author.mention}")
+        await get(ctx.guild.channels, id=int(server[0]["logschannel"])).send(embed=embed)
+
+        
+def setup(client):
+    client.add_cog(Fun(client))
