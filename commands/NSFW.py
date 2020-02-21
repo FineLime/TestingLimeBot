448541bea5ce06112601a768c1cf9cd8022daa10
FineLime <42850488+FineLime@users.noreply.github.com>
2020-02-21 22:33:29 +0000

    Create NSFW.py

---
+++ commands/NSFW.py
@@ -0,0 +1,23 @@
+import discord
+import yippi
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import random
+
+class Nsfw(commands.Cog): 
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.cooldown(1, 10, BucketType.user)
+    async def e621(self, ctx, *, tags): 
+      if ctx.channel.is_nsfw(): 
+        results = yippi.search(tags.split(" "))
+        await ctx.send(random.choice(results).file_url)
+        
+      
+
+        
+def setup(client):
+    client.add_cog(Nsfw(client))
