27cc64fbb2facdcbaddee8c808263484ee55ccec
FineLime <42850488+FineLime@users.noreply.github.com>
2020-02-22 11:17:18 +0000

    Update NSFW.py

--- commands/NSFW.py
+++ commands/NSFW.py
@@ -1,8 +1,10 @@
 import discord
 import yippi
+import rule34
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 import random
+rule34 = rule34.Sync()
 
 class Nsfw(commands.Cog): 
 
@@ -16,6 +18,13 @@ class Nsfw(commands.Cog):
         search = tags.split()
         results = yippi.search.post(search)
         await ctx.send(random.choice(results).file_url)
+    
+    @commands.command()
+    @commands.cooldown(1, 1, BucketType.user)
+    async def rule34(self, ctx, *, tags): 
+      if ctx.channel.is_nsfw(): 
+        images = rule34.getImages(tags=tags)
+        await ctx.send(random.choice(images))
         
       
 
