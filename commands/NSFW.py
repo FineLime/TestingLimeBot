c0a2c4a737e243618d796edcd2959c074fbd1dde
FineLime <42850488+FineLime@users.noreply.github.com>
2020-02-22 11:42:01 +0000

    Update NSFW.py

--- commands/NSFW.py
+++ commands/NSFW.py
@@ -1,5 +1,6 @@
 import discord
 import yippi
+import rule34
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 import random, requests
@@ -22,27 +23,9 @@ class Nsfw(commands.Cog):
     @commands.cooldown(1, 1, BucketType.user)
     async def rule34(self, ctx, *, tags): 
       if ctx.channel.is_nsfw(): 
-        await ctx.channel.trigger_typing()
-        try:
-            data = requests.get(
-                "http://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&limit={}&tags={}".format(tags),
-                headers={"User-Agent": "linux:memebot:v1.0.0"})
-        except json.JSONDecodeError:
-            await ctx.send(("nsfw.no_results_found", ctx).format(tags))
-            return
-
-        count = len(data)
-        if count == 0:
-            await ctx.send(("nsfw.no_results_found", ctx).format(tags))
-            return
-        image_count = 4
-        if count < 4:
-            image_count = count
-        images = []
-        for i in range(image_count):
-            image = data[random.randint(0, count)]
-            images.append("http://img.rule34.xxx/images/{}/{}".format(image["directory"], image["image"]))
-        await ctx.send(("nsfw.results", ctx).format(image_count, count, tags, "\n".join(images)))
+        rule34 = rule34.Rule34(self.client.loop)
+        images = await rule34.getImagesURL(tags=tags)
+        await ctx.send(random.choice(images))
         
       
 
