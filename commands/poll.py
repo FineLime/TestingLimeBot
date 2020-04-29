3fbe0adf6f0ab1bbac5552eb00f5cc6063fede06
FineLime <42850488+FineLime@users.noreply.github.com>
2020-04-29 15:53:24 +0100

    Update poll.py

--- commands/poll.py
+++ commands/poll.py
@@ -10,15 +10,15 @@ class Poll(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def poll(self, ctx, *, poll): 
-        #Poll format: ;poll Should x be allowed to join? | :Zero:=Yes :One:=No
+        #Poll format: ;poll Should x be allowed to join? | :Zero:=Yes / :One:=No /
         poll = poll.split(" | ")
         pollmessage = poll[0]
-        poll = poll[1].split(" ")
+        poll = poll[1].split(" / ")
         reactions = []
         for i in poll: 
             a = i.split("=")
-            pollmessage += f"\n{a[0]} {a[1]}"
-            reactions.append(f"{a[0]}")
+            pollmessage += f"\n{a[0].strip()} {a[1].strip()}"
+            reactions.append(f"{a[0].strip()}")
         reactto = await ctx.send(pollmessage)
         for i in reactions:
             await reactto.add_reaction(i)
