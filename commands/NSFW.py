4f421767e6658025b91df99523546e55f181ff62
FineLime <42850488+FineLime@users.noreply.github.com>
2020-05-18 00:57:59 +0100

    Update NSFW.py

--- commands/NSFW.py
+++ commands/NSFW.py
@@ -1,5 +1,4 @@
 import discord
-import yippi
 import rule34
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
@@ -14,9 +13,15 @@ class Nsfw(commands.Cog):
     @commands.command()
     async def e621(self, ctx, *, tags): 
       if ctx.channel.is_nsfw(): 
-        search = tags.split()
-        results = yippi.search.post(search)
-        await ctx.send(random.choice(results).file_url)
+        search = tags.replace(' ', '+')
+        headers = {
+            
+            'User-Agent'='LimeBot (By FineLime)'   
+            
+        }
+        r = requests.get(f'https://e621.net/posts.json?tags={search}&limit=50', headers=headers, auth=('FineLime', 'yjWn25kgWyi5sZepWVBezW2n'))
+        posts = r.json()['posts']
+        await ctx.send(random.choice(posts)['url'])
     
     @commands.command()
     async def rule34(self, ctx, *, tags): 
