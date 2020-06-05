75346ec11c964ec4f09ba2f9cb3b8236cc4b79f2
FineLime <42850488+FineLime@users.noreply.github.com>
2020-06-05 17:53:39 +0100

    Update purge.py

--- commands/purge.py
+++ commands/purge.py
@@ -13,12 +13,42 @@ class Purge(commands.Cog):
     
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def purgebanana(self, ctx, amount:int, text): 
+    async def purgebanana(self, ctx, type, amount:int, *, text): 
         
-        def isText(m): 
-            return m.content == text
+        await ctx.delete()
+        type = type.lower()
+        text = text.lower()
+        if type == 'match':
+            def isText(m): 
+                return m.content.lower() == text
        
-        await ctx.channel.purge(limit=amount, check=isText)
+            await ctx.channel.purge(limit=amount, check=isText)
+        
+        elif type == 'contains':
+            def hasText(m): 
+                return text in m.content.lower
+            
+            await ctx.channel.purge(limit=amount, check=hasText)
+            
+        elif type == 'banmatch'
+            def banText(m): 
+                if m.content.lower() == text: 
+                    try:
+                        await m.author.ban()
+                    except:
+                        pass
+                    return True
+            await ctx.channel.purge(limit=amount, check=banText)
+            
+        elif type == 'bancontain'
+            def banContains(m): 
+                if text in m.content.lower: 
+                    try:
+                        await m.author.ban()
+                    except:
+                        pass
+                    return True
+            await ctx.channel.purge(limit=amount, check=banContains)
         
 def setup(client):
     client.add_cog(Purge(client))
