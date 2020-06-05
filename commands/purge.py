d33ff4a066f1ace69a608f333c581db35d6ce244
FineLime <42850488+FineLime@users.noreply.github.com>
2020-06-05 17:02:23 +0100

    Update purge.py

--- commands/purge.py
+++ commands/purge.py
@@ -5,12 +5,19 @@ class Purge(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-
+    
+    def banText(m, text): 
+        return m.message == text
+        
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def purge(self, ctx, amount=1): 
         await ctx.channel.purge(limit=amount)
-
+    
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def purgebanana(self, ctx, amount=1): 
+        await ctx.channel.purge(limit=amount, check=banText("banana"))
         
 def setup(client):
     client.add_cog(Purge(client))
