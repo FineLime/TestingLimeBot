1a285d846f36c5e89a603350db7c6970e5c6140b
FineLime <42850488+FineLime@users.noreply.github.com>
2020-07-20 00:36:39 +0100

    Update Fun.py

--- commands/Fun.py
+++ commands/Fun.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 from datetime import datetime
 import requests
+import math
 
 class Fun(commands.Cog): 
 
@@ -30,8 +31,14 @@ class Fun(commands.Cog):
     @commands.cooldown(1, 10, BucketType.user)
     async def launch(self, ctx):
         r = requests.get('https://api.spacexdata.com/v3/launches/next').json()
-        time = datetime.utcfromtimestamp(r['launch_date_unix']).strftime('%B %d, %Y at %I:%M%p')
-        await ctx.send(f'Mission {r["mission_name"]} is set to launch on {time} UTC.\nThe mission: {r["details"]}')
+        time = datetime.utcfromtimestamp(r['launch_date_unix'])
+        e = time - datetime.now()
+        e = divmod(e.days * 86400 + difference.seconds, 60);
+        days = math.floor(e[0]/1440)
+        e[0] -= days*1440
+        hours = math.floor(e[0]/60)
+        e[0] -= hours*60
+        await ctx.send(f'Mission {r["mission_name"]} is set to launch on {time.strftime('%B %d, %Y at %I:%M%p')} (in {days} days, {hours} hours, {e[0]} minutes and {e[1]} seconds) UTC.\nThe mission: {r["details"]}')
 
         
 def setup(client):
