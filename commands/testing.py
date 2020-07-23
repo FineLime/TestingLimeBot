d5ebf681261accf03117243571fa70fccecfec15
FineLime <42850488+FineLime@users.noreply.github.com>
2020-07-23 23:25:28 +0100

    Create testing.py

---
+++ commands/testing.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+
+class Testing(commands.Cog): 
+
+    def __init__(self, client):
+        self.client = client
+
+    @client.group()
+    @commands.cooldown(1, 10, BucketType.user)
+    async def test(self, ctx): 
+        if ctx.invoked_subcommand is None:
+            await ctx.send('You did not use a subcommand')
+            
+    @test.command()
+    async def this(self, ctx):
+        await ctx.send('You used the \'this\' subcommand')
+          
+    @test.command()
+    async def that(self, ctx):
+        await ctx.send('You used the \'that\' subcommand')   
+        
+def setup(client):
+    client.add_cog(Testing(client))
