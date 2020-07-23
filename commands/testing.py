1b6aa4f87bcaadc3112cacdc75306ea029c98bbc
FineLime <42850488+FineLime@users.noreply.github.com>
2020-07-23 23:44:26 +0100

    Update testing.py

--- commands/testing.py
+++ commands/testing.py
@@ -21,5 +21,15 @@ class Testing(commands.Cog):
     async def that(self, ctx):
         await ctx.send('You used the \'that\' subcommand')   
         
+    @commands.group(pass_context=True)
+    @commands.cooldown(1, 10, BucketType.user)
+    async def test2(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('You did not use a subcommand on test 2')
+            
+    @test2.command()
+    async def sub(self, ctx):
+        await ctx.send('You did use a subcommand on test 2')   
+        
 def setup(client):
     client.add_cog(Testing(client))
