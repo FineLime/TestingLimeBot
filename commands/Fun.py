f51776700e50964c24e90048de1e74ff2045ff9c
FineLime <42850488+FineLime@users.noreply.github.com>
2020-07-23 16:15:49 +0100

    Update Fun.py

--- commands/Fun.py
+++ commands/Fun.py
@@ -81,7 +81,7 @@ class Fun(commands.Cog):
         if status == 3:
             displayTime = '- Successful'
             T =  'Launched Successfully'
-        if time == 'TBD':
+        else if time == 'TBD':
             displayTime = '- TBD'     
             T = 'Launch Time - TBD'
         else: 
