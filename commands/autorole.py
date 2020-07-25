f8a3eb1573a63380e2cfd12ecaf149df38e6a5f3
FineLime <42850488+FineLime@users.noreply.github.com>
2020-07-25 02:52:09 +0100

    Create autorole.py

---
+++ commands/autorole.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import asyncpg
+
+class Autorole(commands.Cog): 
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.group(invoke_without_command=True)
+    @commands.has_permissions(manage_roles=True)
+    async def autorole(self, ctx): 
+        await ctx.send("**USAGE:** ;autorole [add/remove] [role]")
+        
+    @autorole.command()
+    async def add(self, ctx, role:discord.Role):
+        
+        check = await self.client.pg_con.fetch("SELECT * FROM autorole WHERE server=$1 AND role=$2", str(ctx.guild.id), str(role.id))
+        if check is not None:
+            await ctx.send("That role is already set up as an autorole")
+            return
+        await self.client.pg_con.execute("INSERT INTO reactionroles (server, role) VALUES ($1, $2)", str(ctx.guild.id), str(role.id))
+        await ctx.send("New autorole added! :white_check_mark:")
+        
+    @autorole.command()
+    async def remove(self, ctx, role:discord.Role):
+        await self.client.pg_con.execute("DELETE FROMreactionroles WHERE server=$1 AND role=$2", str(ctx.guild.id), str(role.id))
+        await ctx.send("Removed autorole! :white_check_mark:")
+        
+def setup(client):
+    client.add_cog(Autorole(client))
