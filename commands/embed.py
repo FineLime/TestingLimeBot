ca682062a5583bcf0eae3c16c124dcf51ca2a1d9
FineLime <42850488+FineLime@users.noreply.github.com>
2020-09-29 14:10:27 +0100

    Update embed.py

--- commands/embed.py
+++ commands/embed.py
@@ -9,8 +9,53 @@ class Embed(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def embed(self, ctx, title='Title', description='description', thumbnail='thumbnail', image='image'): 
-        await ctx.send(f'Title: {title} | description: {description} | thumbnail: {thumbnail}| image: {image}')
+    async def embed(self, ctx, *, p): 
+        ps = p.split('=["')
+        ce = ""
+        t = ""
+        title = "I didn't set a title!"
+        description = "I didn't set a description!"
+        image = ""
+        footer = ""
+        
+        for i in range(len(ps)):
+            if i == 0:
+                ce = ps[1].split(']"')
+                if ps[0].strip().lower() == 'title':
+                    title = ce[0]
+                elif ps[0].strip().lower() == 'description':
+                    description = ce[0]
+                elif ps[0].strip().lower() == 'image':
+                    image = ce[0]
+                elif ps[0].strip().lower() == 'footer':
+                    footer = ce[0]
+            elif i == len(ps):
+                break;
+            else:
+                ce = ps[i].split('"]')[1].strip().lower()
+                t = ps[i+1].split(
+                if ce == 'title':
+                    title = t
+                elif ce == 'description':
+                    description = t
+                elif ce = 'image':
+                    image = t
+                elif ce = 'footer':
+                    footer = t
+                    
+            embed = discord.Embed(title=title, description=description)
+            if footer != "":
+                embed.set_footer(footer)
+            if image != "":
+                embed.set_image(image)
+                    
+            await ctx.send(embed)
+                 
+                    
+            
+                    
+                
+                
 
         
 def setup(client):
