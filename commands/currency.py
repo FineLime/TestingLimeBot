651164bfc60b972c1fc223eb0fde8a216bd9d3cb
FineLime <42850488+FineLime@users.noreply.github.com>
2021-01-08 00:51:40 +0000

    Update currency.py

--- commands/currency.py
+++ commands/currency.py
@@ -26,7 +26,7 @@ class Currency(commands.Cog):
             
         user = await self.client.pg_con.fetch("SELECT * FROM users WHERE serverid=$1 AND userid=$2", str(message.guild.id), str(author.id))
         if (int(time.time()) - int(user[0]["time"])) > 60: 
-            await self.client.pg_con.execute("UPDATE Users SET coins = $1, time = $2 WHERE userid = $3 AND serverid = $4", user[0]['coins']+random.randint(20, 100), str(int(time.time())), author.id, message.guild.id)
+            await self.client.pg_con.execute("UPDATE Users SET coins = $1, time = $2 WHERE userid = $3 AND serverid = $4", user[0]['coins']+random.randint(20, 100), str(int(time.time())), str(author.id), str(message.guild.id))
             
     
     @commands.command()
