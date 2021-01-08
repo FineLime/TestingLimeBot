985762db019f2eac7507a08708f4478d74f2d7d3
FineLime <42850488+FineLime@users.noreply.github.com>
2021-01-08 03:33:00 +0000

    Update coinflip.py

--- commands/coinflip.py
+++ commands/coinflip.py
@@ -7,11 +7,13 @@ class Coinflip(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-
+    
+    '''
     @commands.command(aliases=["flip", "coin", "flipcoin"])
     @commands.cooldown(1, 10, BucketType.user)
     async def coinflip(self, ctx): 
         await ctx.send(f'I flipped a coin and it landed on {random.choice(["heads", "tails"])}')
+    '''
 
         
 def setup(client):
