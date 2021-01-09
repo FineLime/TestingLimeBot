1b6831152a65ac455d380a2e8db98568ece0f3b0
FineLime <42850488+FineLime@users.noreply.github.com>
2021-01-09 02:18:36 +0000

    Update currency.py

--- commands/currency.py
+++ commands/currency.py
@@ -75,7 +75,7 @@ class Currency(commands.Cog):
         
         message += f"\n\n**{ctx.author.name.upper}'s CARDS:**"
         
-        message += f"\n{users_Cards[0]} {users_cards[1]} (Total: {users_total}){chr(10) + 'A natural BlackJack.' if users_total == 21 else ''}"
+        message += f"\n{users_cards[0]} {users_cards[1]} (Total: {users_total}){chr(10) + 'A natural BlackJack.' if users_total == 21 else ''}"
         
         if dealers_total == users_total == 21:  
             message += "\n\nTIE"
