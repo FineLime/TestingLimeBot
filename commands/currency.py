eb2ec09a1999c7999b142b92dbe77c2e7d2182bd
FineLime <42850488+FineLime@users.noreply.github.com>
2021-01-13 18:27:21 +0000

    Update currency.py

--- commands/currency.py
+++ commands/currency.py
@@ -49,7 +49,7 @@ class Currency(commands.Cog):
         await self.client.pg_con.execute("UPDATE users SET coins = coins + $1 WHERE userid = $2 AND serverid = $3", coins, str(user.id), str(ctx.guild.id))
         await ctx.send("Done")
         
-    @commands.group()
+    @commands.group(invoke_without_command=True)
     @commands.cooldown(1, 10, BucketType.user)
     async def lottery(self, ctx):
         await ctx.send("type `;lottery buy` to buy a ticket")
@@ -60,7 +60,7 @@ class Currency(commands.Cog):
         pot = len(tickets)*100
         await ctx.send(f"Current lottery pot is: {pot}")
         
-    @lottery.command(pass_context=True)
+    @lottery.command()
     async def buy(self, ctx):
         user = await self.client.pg_con.fetch("SELECT * FROM users WHERE serverid=$1 AND userid=$2", str(ctx.guild.id), str(ctx.author.id))
         if len(user) == 0:
@@ -906,39 +906,6 @@ class Currency(commands.Cog):
             message += f"[{i['itemid']}] {i['itemname']}\n"
         embed = discord.Embed(title=f"{ctx.author.name}'s Inventory", description=message)
         await ctx.send(embed=embed)
-                           
-                           
-    @commands.group()
-    async def shop(self, ctx):
-        pass
-    
-    @shop.command()
-    @commands.cooldown(1, 5, BucketType.user)
-    async def buy(self, ctx, *, i:str): 
-        user = await self.client.pg_con.fetch("SELECT * FROM users WHERE userid = $1 AND serverid = $2", str(ctx.author.id), str(ctx.guild.id))
-        if len(user) == 0:
-            await ctx.send("Silence, I don't want to hear what you want to buy, I can see you have no money")
-            return
-                           
-        item = await self.client.pg_con.fetch("SELECT * FROM items WHERE itemname = $1", i)
-        if len(item) == 0:
-            try:
-                item = await self.client.pg_con.fetch("SELECT * FROM items WHERE itemid = $1", int(i))
-            except Exception as e:
-                print(e)
-                return
-        if len(item) == 0:
-            await ctx.send("Could not find that item in the shop")
-            return
-                             
-        if user[0]['coins'] >= item[0]['price']:
-            await self.client.pg_con.execute("UPDATE users SET coins = coins - $1 WHERE userid = $2 AND serverid = $3", item[0]['price'], str(ctx.author.id), str(ctx.guild.id))
-            await self.client.pg_con.execute("INSERT INTO useritems (userid, serverid, itemid, itemname) VALUES ($1, $2, $3, $4)", user[0]['userid'], user[0]['serverid'], item[0]['itemid'], item[0]['itemname'])
-            await ctx.send(f"Purchased {item[0]['itemname']}!")
-        else:
-            await ctx.send("You can't afford that.")
-                           
-        
             
                            
 def setup(client):
