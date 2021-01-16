ba9cc202b360032e7f525e78e0a55f69d6f07ebe
FineLime <42850488+FineLime@users.noreply.github.com>
2021-01-16 23:19:31 +0000

    Update voice.py

--- commands/voice.py
+++ commands/voice.py
@@ -19,7 +19,7 @@ class Voice(commands.Cog):
         video_id = response['items'][0]['id']['videoId']
         
         voice_channel = ctx.author.voice.channel
-        vc = await client.join_voice_channel(voice_channel)
+        vc = await self.client.join_voice_channel(voice_channel)
         print(vc)
         player = await vc.create_ytdl_player(f"https://www.youtube.com/watch?v={video_id}")
         player.start()
