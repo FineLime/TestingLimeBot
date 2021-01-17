83909a02158f8781fcdaa704ebdb73db1cd06e27
FineLime <42850488+FineLime@users.noreply.github.com>
2021-01-17 01:11:15 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,19 +8,6 @@ from googleapiclient.discovery import build
 import youtube_dl
 import ctypes
 import ctypes.util
- 
-print("ctypes - Find opus:")
-a = ctypes.util.find_library('opus')
-print(a)
- 
-print("Discord - Load Opus:")
-b = discord.opus.load_opus(a)
-print(b)
- 
-print("Discord - Is loaded:")
-c = discord.opus.is_loaded()
-print(c)
-
 
 MyDB = os.getenv('DATABASE_URL')
 DB = MyDB.split(":")
