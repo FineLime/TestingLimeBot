109a8c58858dc986d7065f0165b464e986f114d2
FineLime <42850488+FineLime@users.noreply.github.com>
2021-01-17 00:18:04 +0000

    Update voice.py

--- commands/voice.py
+++ commands/voice.py
@@ -4,25 +4,25 @@ from discord.ext.commands.cooldowns import BucketType
 
 class Voice(commands.Cog): 
 
-    def __init__(self, client):
-        self.client = client
+	def __init__(self, client):
+		self.client = client
 
-    @commands.command()
-    @commands.cooldown(1, 10, BucketType.user)
-    async def play(self, ctx, *, s): 
-	request = self.client.youtube.search().list(
-		maxResults=1,
-		q=s,
-		part="snippet"
-	)
-	response = request.execute()
-	video_id = response['items'][0]['id']['videoId']
-       	
-	voice_channel = ctx.author.voice.channel
-	vc = await voice_channel.connect()
-	print(vc)
-	player = await YTDLSource.from_url(f"https://www.youtube.com/watch?v={video_id}", loop=self.client.loop, stream=True)
-	ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+	@commands.command()
+	@commands.cooldown(1, 10, BucketType.user)
+	async def play(self, ctx, *, s): 
+		request = self.client.youtube.search().list(
+			maxResults=1,
+			q=s,
+			part="snippet"
+		)
+		response = request.execute()
+		video_id = response['items'][0]['id']['videoId']
+
+		voice_channel = ctx.author.voice.channel
+		vc = await voice_channel.connect()
+		print(vc)
+		player = await YTDLSource.from_url(f"https://www.youtube.com/watch?v={video_id}", loop=self.client.loop, stream=True)
+		ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
 
         
