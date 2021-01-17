4f26ef6f3720c45dd05f7242d8777b0012ddd4e4
FineLime <42850488+FineLime@users.noreply.github.com>
2021-01-17 01:26:51 +0000

    Update voice.py

--- commands/voice.py
+++ commands/voice.py
@@ -18,13 +18,21 @@ class Voice(commands.Cog):
 		response = request.execute()
 		video_id = response['items'][0]['id']['videoId']
 
-		voice_channel = ctx.author.voice.channel
-		vc = await voice_channel.connect()
-		print(vc)
 		async with ctx.typing():
 			player = await self.client.yt_play.from_url(f"https://www.youtube.com/watch?v={video_id}", loop=self.client.loop, stream=True)
 			ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 		await ctx.send("Playing")
+		
+	@play.before_invoke
+	async def ensure_voice(self, ctx):
+		if ctx.voice_client is None:
+			if ctx.author.voice:
+				await ctx.author.voice.channel.connect()
+			else:
+				await ctx.send("You are not connected to a voice channel.")
+				raise commands.CommandError("Author not connected to a voice channel.")
+		elif ctx.voice_client.is_playing():
+			ctx.voice_client.stop()
 
 
         
