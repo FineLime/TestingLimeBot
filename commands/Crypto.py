2bafdf480e38da77e8901f2d772f41690ac3306f
FineLime <42850488+FineLime@users.noreply.github.com>
2021-05-03 12:10:22 +0100

    Update Crypto.py

--- commands/Crypto.py
+++ commands/Crypto.py
@@ -12,13 +12,13 @@ class Crypto(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 10, BucketType.user)
     async def coin(self, ctx, c): 
-        crypto = r.get(f'https://api.binance.com/api/v3/avgPrice?symbol={c.upper()}USDT')
+        crypto = requests.get(f'https://api.binance.com/api/v3/avgPrice?symbol={c.upper()}USDT')
         if crypto.status_code != 200: 
             await ctx.send("An error occurred trying to retrieve this crypto, please make sure it exists.")
             return
             
         price = json.loads(crypto.content)['price']
-        await ctx.send(f"The average price of {c.upper()} over the last 5 minutes is ${price}."}
+        await ctx.send(f"The average price of {c.upper()} over the last 5 minutes is \${price}."}
 
         
 def setup(client):
