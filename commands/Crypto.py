cdb28451a537749f1f76926a15647ba3e27b13fe
FineLime <42850488+FineLime@users.noreply.github.com>
2021-05-03 15:23:17 +0100

    Update Crypto.py

--- commands/Crypto.py
+++ commands/Crypto.py
@@ -30,7 +30,10 @@ class Crypto(commands.Cog):
         
         msg = ""
         for i in coins: 
-            msg += f'Crypto: {i["crypto"]} - Amount: {i["amount"]} \n'
+            crypto = requests.get(f'https://api.binance.com/api/v3/avgPrice?symbol={i["crypto"]}USDT')
+            price = float(json.loads(crypto.content)['price'])
+            total = "{:.5f}".format(i["amount"]*float(price))
+            msg += f'Crypto: {i["crypto"]} - Price: {price} - Amount: {i["amount"]} - Total: ${total} \n'
                 
         await ctx.send(f"Your wallet: \n\n{msg}")
         
@@ -59,6 +62,30 @@ class Crypto(commands.Cog):
         await self.client.pg_con.execute("UPDATE users SET coins = coins - $1 WHERE userid = $2 AND serverid = $3", lcoins, str(ctx.author.id), str(ctx.guild.id))
         await ctx.send(f"Bought {amount} {c} for {lcoins}!")
 
+    @commands.command()
+    @commands.cooldown(1, 10, BucketType.user)
+    async def sell(self, ctx, c, amount:float): 
+        
+        if len(str(amount).split('.')[1]) > 5: 
+            await ctx.send("Max 5 decimal places")
+            return 
+        
+        wallet = await self.client.pg_con.fetch("SELECT * FROM crypto WHERE serverid=$1 AND userid=$2 AND crypto = $3", str(ctx.guild.id), str(ctx.author.id), c.upper())
+        if len(wallet) == 0: 
+            await ctx.send("Crypto not found in your wallet") 
+            return
+        
+        if amount > wallet[0]['amount']: 
+            await ctx.send(f"You don't have that much {c.upper()}.")
+            return
+        
+        crypto = requests.get(f'https://api.binance.com/api/v3/avgPrice?symbol={c.upper()}USDT')
+        price = json.loads(crypto.content)['price'] 
+        lcoins = float("{:.5f}".format(amount*float(price)))
+        
+        await self.client.pg_con.execute("UPDATE crypto SET amount = amount - $1 WHERE userid = $2 AND serverid = $3 AND crypto = $4", amount, str(ctx.author.id), str(ctx.guild.id), c.upper())
+        await self.client.pg_con.execute("UPDATE users SET coins = coins + $1 WHERE userid = $2 AND serverid = $3", lcoins, str(ctx.author.id), str(ctx.guild.id))
+        await ctx.send(f"Sold {amount} {c.upper} for {lcoins} coins.") 
         
 def setup(client):
     client.add_cog(Crypto(client))
