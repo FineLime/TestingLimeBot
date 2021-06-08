1a317b4bd35a74d9509d0fd49e68884c944f51cb
FineLime <42850488+FineLime@users.noreply.github.com>
2021-06-08 16:26:51 +0100

    Create AutoMod.py

---
+++ commands/AutoMod.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+
+class AutoMod(commands.Cog): 
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+	async def on_member_join(self, member): 
+		if "twitter.com/h0nde" in member.name: 
+			try:
+				await member.ban(reason="spam account") 
+			except: 
+				pass
+
+        
+def setup(client):
+    client.add_cog(AutoMod(client))
