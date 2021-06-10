a119dd96fbb937f6402de101ea0b4a1bdca781a2
FineLime <42850488+FineLime@users.noreply.github.com>
2021-06-11 00:06:06 +0100

    Update FFmpeg.py

--- commands/FFmpeg.py
+++ commands/FFmpeg.py
@@ -27,12 +27,12 @@ class FFmpeg(commands.Cog):
 		
 		filename = f'{ctx.message.attachments[0].filename.split(".")[0]}_{random.randint(100, 1000)}_optimized'
 		
-		async with ctx.channel.typing()
+		async with ctx.channel.typing():
 			process = (
-			ffmpeg
-			.input("http://cdn.discordapp.com/attachments/481802328702189569/852674636219023420/4efgwu78a0s51.mp4")
-			.output(discord.File(filename), format='mp4', vcodec="libx264", crf="28")
-			.run_async()
+				ffmpeg
+				.input("http://cdn.discordapp.com/attachments/481802328702189569/852674636219023420/4efgwu78a0s51.mp4")
+				.output(discord.File(filename), format='mp4', vcodec="libx264", crf="28")
+				.run_async()
 			)
 			asyncio.sleep(10)
 			await ctx.send(file=filename)
