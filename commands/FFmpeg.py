c21c22d4b0139b7f8ac67e04ade4b89d41207b28
FineLime <42850488+FineLime@users.noreply.github.com>
2021-06-11 00:03:26 +0100

    Update FFmpeg.py

--- commands/FFmpeg.py
+++ commands/FFmpeg.py
@@ -4,6 +4,7 @@ from discord.ext.commands.cooldowns import BucketType
 import random
 import ffmpeg
 import os
+import asyncio
 
 class FFmpeg(commands.Cog): 
 
@@ -25,14 +26,17 @@ class FFmpeg(commands.Cog):
 			return
 		
 		filename = f'{ctx.message.attachments[0].filename.split(".")[0]}_{random.randint(100, 1000)}_optimized'
-		process = (
-    		ffmpeg
-    		.input("http://cdn.discordapp.com/attachments/481802328702189569/852674636219023420/4efgwu78a0s51.mp4")
-    		.output(discord.File(filename), format='mp4', vcodec="libx264", crf="28")
-    		.run_async()
-		)
-		await ctx.send(file=filename)
-		os.remove(filename)
+		
+		async with ctx.channel.typing()
+			process = (
+			ffmpeg
+			.input("http://cdn.discordapp.com/attachments/481802328702189569/852674636219023420/4efgwu78a0s51.mp4")
+			.output(discord.File(filename), format='mp4', vcodec="libx264", crf="28")
+			.run_async()
+			)
+			asyncio.sleep(10)
+			await ctx.send(file=filename)
+			os.remove(filename)
 
         
 def setup(client):
