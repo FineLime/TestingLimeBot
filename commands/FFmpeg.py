56d3a3e81898050b5c5f817ab5593d2d011bbd92
FineLime <42850488+FineLime@users.noreply.github.com>
2021-06-10 23:56:09 +0100

    Update FFmpeg.py

--- commands/FFmpeg.py
+++ commands/FFmpeg.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 import random
 import ffmpeg
+import os
 
 class FFmpeg(commands.Cog): 
 
@@ -27,10 +28,11 @@ class FFmpeg(commands.Cog):
 		process = (
     		ffmpeg
     		.input("http://cdn.discordapp.com/attachments/481802328702189569/852674636219023420/4efgwu78a0s51.mp4")
-    		.output(filename, format='mp4', vcodec="libx264", crf="28")
+    		.output(discord.File(filename), format='mp4', vcodec="libx264", crf="28")
     		.run_async()
 		)
 		await ctx.send(file=filename)
+		os.remove(filename)
 
         
 def setup(client):
