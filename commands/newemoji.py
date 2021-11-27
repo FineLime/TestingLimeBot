42875833007ecf7eec74d684ca8247f2b5f64d9a
FineLime <42850488+FineLime@users.noreply.github.com>
2021-11-27 00:58:18 +0000

    Update newemoji.py

--- commands/newemoji.py
+++ commands/newemoji.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
+from PIL import Image
+import requests
+from io import BytesIO
 
 class NewEmoji(commands.Cog): 
 
